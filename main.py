bbe416f13db6645bda171c8383ffcb755e07a5c9
Johan Nenzén <johan.nanzen@gmail.com>
2020-03-13 10:37:08 +0100

    Adds more pins for Keg

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import asyncio
 
 from pyplaato.plaato import (
     PlaatoType,
-    get_airlock_data
+    get_airlock_data,
+    get_keg_data
 )
 
 
 async def go():
     async with aiohttp.ClientSession() as session:
         device = await get_airlock_data(session, "")
+        keg = await get_keg_data(session, "")
         print(device)
 
 
--- pyplaato/plaato.py
+++ pyplaato/plaato.py
@@ -1,4 +1,6 @@
 """Fetch data from Plaato Airlock and Keg"""
+from datetime import datetime
+
 from aiohttp import ClientSession
 from dataclasses import dataclass
 from enum import Enum
@@ -24,18 +26,66 @@ class PlaatoKeg:
     """Class for holding a Plaato Keg"""
 
     def __init__(self, attrs):
-        self.__percent_beer_left = attrs[self.Pins.PERCENT_BEER_LEFT]
+        self.beer_left_unit = attrs[self.Pins.BEER_LEFT_UNIT] or None
+        self.volume_unit = attrs[self.Pins.VOLUME_UNIT] or None
+        self.measure_unit = attrs[self.Pins.MEASURE_UNIT] or None
+        self.name = attrs[self.Pins.BEER_NAME] or "Beer"
+        self.__percent_beer_left = attrs[self.Pins.PERCENT_BEER_LEFT] or None
         self.__pouring = attrs[self.Pins.POURING]
-        self.__beer_left = attrs[self.Pins.BEER_LEFT]
-        self.__temperature = attrs[self.Pins.TEMPERATURE]
+        self.__beer_left = attrs[self.Pins.BEER_LEFT] or None
+        self.__temperature = attrs[self.Pins.TEMPERATURE] or None
         self.__unit_type = attrs[self.Pins.UNIT_TYPE]
+        self.__last_pour = attrs[self.Pins.LAST_POUR] or None
+        self.__date = attrs[self.Pins.DATE] or None
+
+    @property
+    def date(self):
+        if self.__date is not None:
+            date = datetime.strptime(self.__date, "%m/%d/%Y")
+            return datetime.timestamp(date)
+        return None
+
+    @property
+    def temperature(self):
+        if self.__temperature is not None:
+            return round(self.__temperature, 2)
+
+    @property
+    def beer_left(self):
+        if self.__beer_left is not None:
+            return round(self.__beer_left, 2)
+
+    @property
+    def percent_beer_left(self):
+        if self.__percent_beer_left is not None:
+            return round(self.__percent_beer_left, 2)
+
+    @property
+    def last_pour(self):
+        if self.__last_pour is not None:
+            return round(self.__last_pour, 2)
+
+    @property
+    def pouring(self):
+        """
+        1 = Pouring
+        255 = Not Pouring
+        :return: True if 1 = Pouring else False
+        """
+        return self.__pouring is "1"
 
     class Pins(Enum):
+        BEER_NAME = "v64"
         PERCENT_BEER_LEFT = "v48"
         POURING = "v49"
         BEER_LEFT = "v51"
+        BEER_LEFT_UNIT = "v74"
         TEMPERATURE = "v56"
         UNIT_TYPE = "v71"
+        MEASURE_UNIT = "v75"
+        VOLUME_UNIT = "v82"
+        LAST_POUR = "v59"
+        DATE = "v67"
 
 
 @dataclass
@@ -43,7 +93,7 @@ class PlaatoAirlock:
     """Class for holding a Plaato Keg"""
 
     def __init__(self, attrs):
-        self.__bmp = attrs[self.Pins.BPM] or None
+        self.bmp = attrs[self.Pins.BPM] or None
         self.__temperature = float(attrs[self.Pins.TEMPERATURE]) or None
 
     def __repr__(self):
@@ -53,10 +103,6 @@ class PlaatoAirlock:
     def temperature(self):
         if self.__temperature is not None:
             return round(self.__temperature, 2)
-
-    @property
-    def bmp(self):
-        return self.__bmp
 
     class Pins(Enum):
         BPM = "v102"
@@ -89,8 +135,3 @@ async def fetch_data(session: ClientSession, url: str, pin: str):
     async with session.get(f"{url}/{pin}") as resp:
         data = await resp.json(content_type=None)
         return data[0] or None
-
-
-
-
-
