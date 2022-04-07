0e00517828d27917118142d041db5b751f542af7
Andreas Billmeier <b@edevau.net>
2022-04-07 22:52:45 +0200

    Update setup.py, exclude 'tests' (#4)
    otherwise it would try to install a package called 'tests' at top level, which is very bad sytle (and forbidden).

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/JohNan/pyplaato",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["tests","tests.*"]),
     classifiers=[
         "Framework :: AsyncIO",
         "Programming Language :: Python :: 3",
