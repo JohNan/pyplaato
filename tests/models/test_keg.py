9180a42141eca42a9e792052eab79c7736fcd299
Johan Nenzén <johan.nanzen@gmail.com>
2022-04-21 23:25:07 +0200

    Fixes parse error when date is only spaces (#6)
    * Fixes parse error when date is only spaces
    
    * Test action fix
    
    * Set timezone in test

--- tests/models/test_keg.py
+++ tests/models/test_keg.py
@@ -1,13 +1,19 @@
+import os, time
 from datetime import datetime
 from unittest import mock
 
 from pyplaato.models.keg import PlaatoKeg
 
 
+def setup_module(module):
+    os.environ['TZ'] = 'Europe/London'
+    time.tzset()
+
+
 def test_date_prop_with_value_returns_its_timestamp():
     pins = PlaatoKeg.Pins
-    keg = PlaatoKeg({pins.DATE: "1/1/2022"})
-    assert 1641024000.0 == keg.date
+    keg = PlaatoKeg({pins.DATE: "10/1/2022"})
+    assert 1664578800.0 == keg.date
 
 
 @mock.patch("pyplaato.models.device.datetime")
@@ -17,3 +23,20 @@ def test_date_prop_with_no_value_returns_current_timestamp(m_datetime):
     keg = PlaatoKeg({})
     assert now.timestamp() == keg.date
 
+
+@mock.patch("pyplaato.models.device.datetime")
+def test_date_prop_with_empty_string(m_datetime):
+    now = datetime(2022, 10, 1)
+    m_datetime.now.return_value = now
+    pins = PlaatoKeg.Pins
+    keg = PlaatoKeg({pins.DATE: ""})
+    assert now.timestamp() == keg.date
+
+
+@mock.patch("pyplaato.models.device.datetime")
+def test_date_prop_with_space_string(m_datetime):
+    now = datetime(2022, 10, 1)
+    m_datetime.now.return_value = now
+    pins = PlaatoKeg.Pins
+    keg = PlaatoKeg({pins.DATE: " "})
+    assert now.timestamp() == keg.date
