d60d5f3cfbee46298377e8c32b32fdc81858f485
Johan Nenzén <johan.nanzen@gmail.com>
2022-04-21 23:41:04 +0200

    Bump version to v0.0.18

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyplaato",
-    version="0.0.17",
+    version="0.0.18",
     author="JohNan",
     author_email="johan.nanzen@gmail.com",
     description="Asynchronous Python client for getting Plaato Airlock and Keg data",
